0cf6ffb6fd4184aafb1c8407ba8d2b796a9607bb
Berita-Winnie <beritawinnie@gmail.com>
2025-11-27 09:29:26 +0300

    feat: hide static weather HTML and show results only after user search

--- weather_app/script.js
+++ weather_app/script.js
@@ -8,8 +8,6 @@ async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
   const data = await response.json()
 
-  console.log(data)
-
   document.querySelector('.city').innerHTML = data.name
   document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C'
   document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
@@ -26,6 +24,8 @@ async function checkWeather(city) {
   } else if (data.weather[0].main === 'Mist') {
     weatherIcon.src = 'images/mist.png'
   }
+
+  document.querySelector('.weather').style.display = 'block'
 }
 
 searchBtn.addEventListener('click', () => {
