2cfeb74701835eca142e868b902d32aecd17200b
Berita-Winnie <beritawinnie@gmail.com>
2026-01-13 16:25:56 +0300

    feat: finalize quote app and enable sharing on X

--- quote_generator/script.js
+++ quote_generator/script.js
@@ -1,13 +1,28 @@
 const api_url = 'https://quoteslate.vercel.app/api/quotes/random'
 const quote = document.getElementById('quote')
 const author = document.getElementById('author')
+const newquotebtn = document.getElementById('newquote')
+const tweetbtn = document.getElementById('tweet')
 
 async function getquote(url) {
   const response = await fetch(url)
   const data = await response.json()
-  console.log(data)
   quote.innerHTML = data.quote
   author.innerHTML = data.author
 }
-
 getquote(api_url)
+
+function tweet() {
+  window.open(
+    (href =
+      'https://twitter.com/intent/tweet?text=' +
+      quote.innerHTML +
+      '---- by' +
+      author.innerHTML),
+    'Tweet Window',
+    'width=600, height=300'
+  )
+}
+
+newquotebtn.addEventListener('click', () => getquote(api_url))
+tweetbtn.addEventListener('click', () => tweet())
