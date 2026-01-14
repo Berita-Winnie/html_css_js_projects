c840af47c3944538a251ec725c0b92013d14a337
Berita-Winnie <beritawinnie@gmail.com>
2026-01-14 18:19:30 +0300

    Used qrcode generator API to genarate various  qr codes by use of different words

--- qr_generator/script.js
+++ qr_generator/script.js
@@ -1,1 +1,13 @@
+let imgBox = document.getElementById('img-box')
+let qrImage = document.getElementById('qrImage')
+let qrText = document.getElementById('qrText')
+let qrBtn = document.getElementById('qrBtn')
 
+function generateQr() {
+  qrImage.src =
+    'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' +
+    qrText.value
+  imgBox.classList.add('show-img')
+}
+
+qrBtn.addEventListener('click', () => generateQr())
