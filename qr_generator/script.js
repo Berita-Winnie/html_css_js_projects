52b7d5078e3ee7adbcc99ba053b7b5adddca95a7
Berita-Winnie <beritawinnie@gmail.com>
2026-01-16 13:37:21 +0300

    Completed qr code generator by setting error boundaries..

--- qr_generator/script.js
+++ qr_generator/script.js
@@ -4,10 +4,16 @@ let qrText = document.getElementById('qrText')
 let qrBtn = document.getElementById('qrBtn')
 
 function generateQr() {
-  qrImage.src =
-    'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' +
-    qrText.value
-  imgBox.classList.add('show-img')
+  if (qrText.value.length > 0) {
+    qrImage.src =
+      'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' +
+      qrText.value
+    imgBox.classList.add('show-img')
+  } else {
+    qrText.classList.add('error')
+    setTimeout(() => {
+      qrBtn.textContent.classList.remove('error')
+    }, 1000)
+  }
 }
-
 qrBtn.addEventListener('click', () => generateQr())
